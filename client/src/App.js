760435b01b91ce4a54978dbb1b24d0f31d7bdbdf
Luke <77371718+ldalton02@users.noreply.github.com>
2021-11-26 15:49:02 -0800

    More changes for the backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import React, {useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 
+import {deleteUser, insertUser} from './backend-calls'
+
 /*to-do
 install react router and make header component + different pages
 */
-
-function App() {
+ 
+  /* FORMAT TO INSERT A USER
   const values = {
     id: 1134,
     status: false,
@@ -19,14 +21,15 @@ function App() {
     known_languages: ['javascript', 'python'],
     projects_worked: [1,3,]
   } 
-  
+  */
 
+function App() {
   useEffect(() => {
-    axios.post('/test', values)
-    .then(res => console.log(res))
+    insertUser()
+    axios.post('/getuserinfo', {username: 'myuser'} )
+    .then(res => console.log(res.data))
     .catch(err => console.log(err))
-  }, )
-
+  }, [])
 
   return (
     <div className="App">
--- server/index.js
+++ server/index.js
@@ -59,33 +59,31 @@ projects_worked
 /*
 insert syntax:
 insert into users(id, status, username, password, email, github, year_exp, known_languages, projects_worked) values(1, false, 'newUser', 'testpass', 'newemail', 'newgithub', 1, '{"hello"}'::varchar[], '{1, 3}'::int[])
-
-
 */
 
-app.get('/getuserinfo', (req,res) => {
+app.post('/getuserinfo', (req,res) => {
   const {username} = req.body;
-  let query = `select * from users where id = ${username}`
+  let query = `select * from users u where u.username = '${username}'`
   client.query(query)
-  .then(res => res.send(res))
+  .then(response => res.send(response))
   .catch(err => console.error(err))
 });
 
-const insertUserQuery = 'INSERT INTO users(id, status, username, password, email, github, year_exp, known_languages, projects_worked) values($1, $2, $3, $4, $5, $6, $7, $8::varchar[], $9::int[])'
+
+
+
+const insertUserQuery = 'INSERT INTO users(status, username, password, email, github, year_exp, known_languages, projects_worked) values($1, $2, $3, $4, $5, $6, $7::varchar[], $8::int[])'
 // note - have to format $8 (known_languages) to be in {1,2,3} format and same for projects_worked
-app.post('/test', (req,res) => { // use req.body to receive params from frontend
+app.post('/insertuser', (req,res) => { // use req.body to receive params from frontend
   console.log(req.body)
-  const { id, status, username, password, email, github, year_exp, known_languages, projects_worked } = req.body;
+  const { status, username, password, email, github, year_exp, known_languages, projects_worked } = req.body;
   /* janky way to get known languages and projects worked array into format to submit */
   known_languages_input = '{'
   known_languages.map(each => {
     known_languages_input += (each + ',')
   })
-
   known_languages_input = known_languages_input.substring(0, known_languages_input.length - 1)
   known_languages_input += '}'
-
-
   projects_worked_input = '{'
   projects_worked.map(each =>{
     projects_worked_input += (each + ',')
@@ -93,11 +91,34 @@ app.post('/test', (req,res) => { // use req.body to receive params from frontend
   projects_worked_input= projects_worked_input.substring(0, projects_worked_input.length - 1)
   projects_worked_input += '}'
 
-  const toInsertVals = [id, status, username, password, email, github, year_exp, known_languages_input, projects_worked_input]
+  const toInsertVals = [status, username, password, email, github, year_exp, known_languages_input, projects_worked_input]
 
   client.query(insertUserQuery, toInsertVals)
-  .then(res => console.log(res))
+  .then(response => res.send(response))
   .catch(err => console.error(err))
 });
 
+
+app.post('/deleteuser', (req, res) => {
+    const {username} = req.body
+    const query = `DELETE FROM users where username = '${username}'` 
+    client.query(query)
+    .then(response => res.send(response))
+    .catch(err => res.send(err))
+})
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+/*
+USERS TABLE
+  note for yash - I set the username to unique key so that database doesn't allow duplicate usernames by default
+  id is also a primary key
+  I also set github to be able to be null - are we requiring github for all users?
+  (note - only username is unique id rn - should I Write SQL to make email unique?)
+
+  database is all done being setup with your structure - only diff is that username is unique key
+
+
+  maybe try to write some stuff here to link projects to users, tasks to projects?
+
+*/
