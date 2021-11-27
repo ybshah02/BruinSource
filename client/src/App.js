db15685b538408b71bd7e18c8cfb73228a30b6ba
Tyler Stovsky <stovsky@ucla.edu>
2021-11-26 19:46:52 -0500

    Created the login page.
    -Added files Login.js, Login.css, and bruinsource-logo.png
    -Updated App.js to use the Login component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import React, {useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
-
+import Login from './Login.js';
 import {deleteUser, insertUser} from './backend-calls'
 
 /*to-do
@@ -33,20 +33,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
     </div>
   );
 }
