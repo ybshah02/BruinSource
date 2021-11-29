560da0a7ab51344923218daa3254102ff707687b
neil <neilkardan@gmail.com>
2021-11-28 20:36:58 -0800

    Created project info page (where users can request to collaborate), and task info page (where users can mark that they've submitted a task).

---
+++ client/src/TaskInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './TaskInfo.css';
+import mainLogo from './bruinsource_logo.png'
+import history from './history.js'
+class TaskInfo extends React.Component {
+    render() {
+        return (
+            <div className="TaskInfo">
+                <img src={mainLogo} className="MainLogo" alt="mainLogo"/>
+                <h1>Project Name</h1>
+                <h2>Task Name</h2>
+                <p>Lorem ipsum dolor amet</p>
+                    <div className="Buttons">
+                        <button type="button" className="BackToTasks" onClick={() => history.push('')}>Back to Tasks</button>
+                        <button type="button" className="SubmitTask">Submit Task</button>
+                    </div>
+            </div>
+        );
+    }
+}
+
+export default TaskInfo;
