7b21193c2967def8df94ac2c6087589deaf34c14
neil <neilkardan@gmail.com>
2021-11-28 20:36:58 -0800

    Created project info page (where users can request to collaborate), and task info page (where users can mark that they've submitted a task).

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -6,6 +6,8 @@ import Dashboard from './Dashboard.js'
 import CreateTask from './CreateTask.js'
 import CreateProject from './CreateProject.js'
 import Register from './Register.js'
+import TaskInfo from './TaskInfo.js'
+import ProjectInfo from './ProjectInfo'
 import history from './history';
 
 export default class Routes extends Component {
@@ -18,6 +20,8 @@ export default class Routes extends Component {
                     <Route exact path="/dashboard" component={Dashboard} />
                     <Route exact path="/createtask" component={CreateTask} />
                     <Route exact path="/createproject" component={CreateProject} /> 
+                    <Route exact path="/taskinfo" component={TaskInfo} />
+                    <Route exact path="/projectinfo" component={ProjectInfo} />
                 </Switch>
             </Router>
         )
