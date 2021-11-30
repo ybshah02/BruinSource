9e14e6b66ca9d5c666de69a2b3e614d26400cded
chelseabiala <74638201+chelseabiala@users.noreply.github.com>
2021-11-30 10:06:31 -0800

    Fixed dynamic table allocation
    edits to Dashboard.js in rendering table data from database

---
+++ client/src/TaskDashBoard.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './TaskDashBoard.css';
+import mainLogo from './bruinsource_logo.png'
+import searchIcon from './search_icon.png'
+import history from './history';
+
+class TaskDashBoard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          tasks: []
+        }
+    }
+    /*Have to find out how to get project id for this particular page*/
+    TaskList() {
+        return fetch('https://localhost:8000/api/projects/:projectId/tasks')
+        .then(res =>this.setState({projects: res.rows}));
+    }
+    renderTableData() {
+        return this.state.tasks.map((task, index) => {
+            const {id, name, description, tags, project_id, assigned_user, date_created, date_due, is_finished, date_finished} = task
+            return (
+                <tr key={name}>
+                    <td>{name}</td>
+                    <td>{assigned_user}</td>
+                    <td>{date_created}</td>
+                    <td>{is_finished}</td>
+                </tr>
+            )
+        })
+    }
+    render() {
+        return (
+            <div className="TaskDashBoard">
+                <img src={mainLogo} className="MainLogo" alt="mainLogo"/>
+                <h2> Tasks </h2>
+                <form>
+                    <input
+                    type="text"
+                    placeholder="Search for a task..."
+                    />
+                </form>
+                <button type="button" className="Search"> 
+                    <img src={searchIcon} width="50px" alt="searchIcon" ></img>
+                </button>
+                <button type="button" className="Create">CREATE A TASK</button>
+                <div className="TaskList">
+                    <table className="TaskListTable">
+                        <thead className="TaskListTableHead">
+                            <tr>
+                                <td>TASK</td>
+                                <td>CONTRIBUTOR</td>
+                                <td>DATE CREATED</td>
+                                <td>STATUS</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderTableData()}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TaskDashBoard;
