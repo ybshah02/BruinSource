785e6c87bc1292393daeed931483750b76cfad07
Tyler Stovsky <stovsky@ucla.edu>
2021-11-30 15:57:53 -0800

    Started implementation for creating a project

--- client/src/CreateProject.js
+++ client/src/CreateProject.js
@@ -2,30 +2,73 @@ import React from 'react';
 import './CreateProject.css';
 import mainLogo from './bruinsource_logo.png'
 import history from './history';
+import axios from 'axios';
 
 class CreateProject extends React.Component {
     constructor(props)
     {
-        super(props)
+        super(props);
+
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+    
+        today = yyyy + '-' + dd + '-' + mm;
+
         this.state = 
         {
             name: null,
             description: null,
             tags: null,
             github: null,
+            author: null,
+            date_created: today,
+            last_updated: today,
+            collaborators: [],
+            requests: [],
+
+            alert: null,
         }
     }
 
-    onCreateProject = () => 
-    {
-        //axios.post('/api/createproject', {name: this.state.name, description: this.state.description, tags: this.state.tags, github: this.state.github})
-        /*
-                    setAlert('Project created successfully! Redirecting...')
-                    setTimeout(() => {
-                        history.push('/dashboard')
-                    }, 5000);
-        */
+ /*   testLogs = () => {
+
+
+        console.log(this.state.name);
+        console.log(this.state.description);
+        console.log(this.state.tags);
+        console.log(this.state.github);
+        console.log(this.state.author);
+        console.log(this.state.date_created);
+        console.log(this.state.last_updated);
+        console.log(this.state.collaborators);
+        console.log(this.state.requests);
     }
+*/
+
+
+     onCreateProject = () => 
+      {
+
+        axios.post('/api/projects/create', 
+        {
+            name: this.state.name, 
+            description: this.state.description, 
+            tags: this.state.tags, 
+            github: this.state.github,
+            date_created: this.state.date_created,
+            last_updated: this.state.last_updated,
+            author: this.state.author,
+            collaborators: this.state.collaborators,
+                requests: this.state.requests,
+        })
+        
+        //        this.setState({
+       //             alert: 'Project created successfully! Redirecting...'
+       //         })
+        
+       }
 
     render() {
         return (
@@ -66,7 +109,7 @@ class CreateProject extends React.Component {
                     </div>
                     <div className="Buttons">
                         <button type="button" className="BackToProjects" onClick={() => history.push('/dashboard')}>Back to Projects</button>
-                        <button type="button" className="CreateNewProject" onClick={() => console.log(this.state.description)}>Create New Project</button>
+                        <button type="button" className="CreateNewProject" onClick={this.onCreateProject()}>Create New Project</button>
                     </div>
                 </form>
             </div>
