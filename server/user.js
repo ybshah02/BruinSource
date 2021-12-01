79316539287d8155699fb5890316eed9ea2c0d32
Luke <77371718+ldalton02@users.noreply.github.com>
2021-12-01 02:02:04 -0800

    User fix

--- server/user.js
+++ server/user.js
@@ -2,7 +2,11 @@ const { client, formatArrayToSql } = require('./db.js')
 const bcrypt = require('bcrypt')
 
 class User {
+<<<<<<< HEAD
     constructor(id, username, password, email, github, known_languages, year_exp){
+=======
+    constructor(id, username, password, email, github, known_languages, year_exp, projects_owned) {
+>>>>>>> ff29b23 (User fix)
         this.id = id;
         this.username = username;
         this.password = password;
@@ -17,11 +21,11 @@ function validateUsername(username) {
     // check if username is null and if it already exists in db
     if (username.length != 0) {
         client
-        .query(`select * from users u where u.username = '${username}'`)
-        .catch(err => {
-            // error means that username doesn't already exist, so chosen username is valid
-            return true;
-        })
+            .query(`select * from users u where u.username = '${username}'`)
+            .catch(err => {
+                // error means that username doesn't already exist, so chosen username is valid
+                return true;
+            })
     } else {
         return true;
     }
@@ -39,27 +43,28 @@ function validateEmail(email) {
 function validatePassword(password) {
     // check if password has at least one upper case character
     const numUpper = password
-                        .split('')
-                        .map(char => /[A-Z]/.test(char))
-                        .reduce((curr,prev) => curr + prev);
+        .split('')
+        .map(char => /[A-Z]/.test(char))
+        .reduce((curr, prev) => curr + prev);
 
     // check if password has at least one special case character
     const numSpecial = password
-                        .split('')
-                        .map(char => /[^a-zA-Z\d]/.test(char))
-                        .reduce((curr,prev) => curr + prev);
+        .split('')
+        .map(char => /[^a-zA-Z\d]/.test(char))
+        .reduce((curr, prev) => curr + prev);
 
     // check i password has at least one number
     const numDigits = password
-                        .split('')
-                        .map(char => /\d/.test(char))
-                        .reduce((curr,prev) => curr + prev);
+        .split('')
+        .map(char => /\d/.test(char))
+        .reduce((curr, prev) => curr + prev);
 
     if (password.length >= 8 && numUpper > 0 && numSpecial > 0 && numDigits > 0) return true;
     else return false;
 }
 
 // adds a user to database and returns the user object in json 
+<<<<<<< HEAD
 async function registerUser(req, res) {
     const { username, 
             password, 
@@ -67,16 +72,28 @@ async function registerUser(req, res) {
             github, 
             year_exp, 
             known_languages
+=======
+function registerUser(req, res) {
+    const { username,
+        password,
+        email,
+        github,
+        year_exp,
+        known_languages,
+        projects_owned
+>>>>>>> ff29b23 (User fix)
     } = req.body;
 
     // validate username input
 
+    let usernameValid = true
+    if (username == '') {
+        usernameValid = false
+    }
+
     // validate password input
     let passwordValid = validatePassword(password);
-    if (passwordValid) {
-        // encrypt password
-        hashed_password = await bcrypt.hash(password, 10)
-    } else {
+    if (!passwordValid) {
         res.status(201).send({ msg: 'invalid_password' })
         return;
     }
@@ -88,26 +105,44 @@ async function registerUser(req, res) {
         return;
     }
 
+
     // set default to empty string
     if (!github) github = '';
 
     // set default to 0 years of experience if no input
     if (!year_exp) year_exp = 0;
 
+<<<<<<< HEAD
     known_languages_input = formatArrayToSql(known_languages);
+=======
+>>>>>>> ff29b23 (User fix)
 
+    if (!known_languages) {
+        res.status(201).send({ msg: 'invalid_languages' })
+        return;
+    }
+    known_languages_input = formatArrayToSql(known_languages);
+   
     // make query if inputs are all valid
+<<<<<<< HEAD
     if (emailValid && passwordValid) { 
         const query = 'INSERT INTO users(status, username, password, email, github, year_exp, known_languages) values($1, $2, $3, $4, $5, $6, $7::varchar[])';
         const vals = [username, hashed_password, email, github, year_exp, known_languages_input];
+=======
+    if (usernameValid && emailValid && passwordValid) {
+        const query = 'INSERT INTO users(status, username, password, email, github, year_exp, known_languages) values($1, $2, $3, $4, $5, $6, $7::varchar[])';
+        const vals = [username, password, email, github, year_exp, known_languages_input];
+>>>>>>> ff29b23 (User fix)
 
         client
-        .query(query,vals)
-        .catch(err => res.status(201).send(err));
+            .query(query, vals)
+            .catch(err => res.status(201).send(err));
+    } else {
+        res.status(201).send({msg: 'something_wrong'});
     }
 }
 
-function validateLogin(req, res){
+function validateLogin(req, res) {
     const { username, password } = req.body;
     const query = `select * from users u where u.username = '${username}'`;
 
@@ -129,21 +164,22 @@ function validateLogin(req, res){
             })
         })
         .catch(err => {
-            res.status(201).send({ msg: 'invalid_username' })})
+            res.status(201).send({ msg: 'invalid_username' })
+        })
 }
 
 // deletes a user from database and returns that user object in json
-function deleteUser(req, res){
+function deleteUser(req, res) {
     const { username } = req.params;
-    const query = `DELETE FROM users u where u.username = '${username}'`; 
-    
+    const query = `DELETE FROM users u where u.username = '${username}'`;
+
     client
-    .query(query)
-    .catch(err => res.status(201).send({msg: 'invalid_username'}));
+        .query(query)
+        .catch(err => res.status(201).send({ msg: 'invalid_username' }));
 }
 
 // returns json of all existing users 
-function getUsers(req, res){
+function getUsers(req, res) {
     const query = `select * from users`;
     client
         .query(query)
@@ -158,13 +194,13 @@ function getUserByUsername(req, res) {
     const { username } = req.params;
     const query = `select * from users u where u.username = '${username}'`;
     client
-    .query(query)
-    .then(user => {
-        res.status(200).send(user.rows[0]);
-    })
-    .catch(err => {
-        res.status(201).send({msg: `invalid_username`});
-    })
+        .query(query)
+        .then(user => {
+            res.status(200).send(user.rows[0]);
+        })
+        .catch(err => {
+            res.status(201).send({ msg: `invalid_username` });
+        })
 }
 
 module.exports = {
