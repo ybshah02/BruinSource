252b1e6019205b54a3287dfc86ae3539427858e1
chelseabiala <74638201+chelseabiala@users.noreply.github.com>
2021-12-01 21:41:55 -0800

    Added header link
    The header looked lonely

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -36,14 +36,18 @@ const Header = (props) => {
                         {!auth.signedIn ?
                             null
                             :
-                            <Button style={{ marginRight: '1em' }} onClick={() => history.push('/searchinterest')} color="inherit" fontFamily="inherit">Search Projects by Tags</Button>
+                            <Button style={{ marginRight: '3em' }} onClick={() => history.push('/searchinterest')} color="inherit" fontFamily="inherit">Search Projects by Tags</Button>
+                        }
+                        {!auth.signedIn ?
+                            null
+                            :
+                            <Button style={{ marginRight: '3em' }} onClick={() => history.push('/dashboard')} color="inherit" fontFamily='inherit'>My Projects</Button>
                         }
                         {!auth.signedIn ?
                             null
                             :
                             <Button style={{ marginRight: '1em' }} onClick={buttonClick} color="inherit" fontFamily='inherit'>Log Out</Button>
                         }
-
                     </Toolbar>
                 </AppBar>
             </Box>
